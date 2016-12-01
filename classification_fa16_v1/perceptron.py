3330fdb391fe3ae1d18a8ff9665e3f4d0387db49
Max Jacob Schridde <nimda@Nimdas-MacBook-Pro.local>
2016-11-30 18:38:13 -0800

    Implemented Q1

--- classification_fa16_v1/perceptron.py
+++ classification_fa16_v1/perceptron.py
@@ -11,7 +11,7 @@ class Perceptron(object):
         self.numFeatures = numFeatures
 
         """YOUR CODE HERE"""
-        raiseNotDefined()
+        self.weightMatrix = np.zeros((len(self.categories), self.numFeatures))
 
 
     def classify(self, sample):
@@ -19,16 +19,26 @@ class Perceptron(object):
            returns: category with maximum score, must be from self.categories"""
 
         """YOUR CODE HERE"""
-        raiseNotDefined()
+        arr = []
+        for i in range(len(self.categories)):
+          arr.append(np.dot(self.weightMatrix[i], sample))
+        return self.categories[np.argmax(arr)]
 
 
     def train(self, samples, labels):
-        """samples: np.array of shape (numFeatures, numSamples)
+        """samples: np.array of shape (numFeatures, numSamples) (numSamples, numFeatures)
            labels: list of numSamples strings, all of which must exist in self.categories 
            performs the weight updating process for perceptrons by iterating over each sample once."""
 
         """YOUR CODE HERE"""
-        raiseNotDefined()
-
+        arr = []
+        for i in range(len(labels)):
+          labelprime = self.classify(samples[i])
+          if labelprime != labels[i]:
+            for j in range(len(self.categories)):
+              if self.categories[j] == labelprime:
+                self.weightMatrix[j] = self.weightMatrix[j] - samples[i]
+              if self.categories[j] == labels[i]:
+                self.weightMatrix[j] = self.weightMatrix[j] + samples[i]
 
 
